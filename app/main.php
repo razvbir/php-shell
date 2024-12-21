8d415c8cde928e5a4e9a92b6cf9da9907f6ed1e2
Răzvan Birișan <razvan.birisan@pm.me>
2024-12-21 15:32:20 +0200

    [any message]

--- app/main.php
+++ app/main.php
@@ -1,10 +1,68 @@
 <?php
+declare(strict_types=1);
 error_reporting(E_ALL);
+
+class CommandNotFoundException extends Exception {
+    const INVALID_COMMAND_MESSAGE = ': command not found';
+    const INVALID_COMMAND_CODE = 404;
+
+    public static function make(string $commandName = ''): static
+    {
+        return new CommandNotFoundException(
+            $commandName.self::INVALID_COMMAND_MESSAGE,
+            self::INVALID_COMMAND_CODE,
+        );
+    }
+};
+
+readonly abstract class Command
+{
+    public function __construct(
+        protected string $commandName,
+        protected array $args = [],
+    ) {
+    }
+
+    abstract public function execute(): void;
+
+    public static function make(string $command): static
+    {
+        $args = explode(" ", trim($command));
+        $commandName = $args[0];
+        $commandArgs = array_slice($args, 1);
+
+        return match ($commandName) {
+            'exit' => new ExitCommand($commandName, $commandArgs),
+            default => throw CommandNotFoundException::make($commandName),
+        };
+    }
+}
+
+readonly class ExitCommand extends Command
+{
+    private int $statusCode;
+
+    public function __construct(
+        protected string $commandName,
+        protected array $args = [],
+    ) {
+        $this->statusCode = (int) ($this->args[0] ?? 0);
+    }
+
+    public function execute(): void
+    {
+        exit($this->statusCode);
+    }
+}
 
 while (true) {
     fwrite(STDOUT, "$ ");
 
     $input = fgets(STDIN);
-    $command = trim($input);
-    fwrite(STDOUT, "$command: command not found" . PHP_EOL);
+    try {
+        $command = Command::make($input);
+        $command->execute();
+    } catch (CommandNotFoundException $e) {
+        fwrite(STDOUT, $e->getMessage() . PHP_EOL);
+    }
 }
