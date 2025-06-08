96428b4dc021304671d69879c3292aaf832c0a41
Răzvan Birișan <razvan.birisan@pm.me>
2025-06-08 16:23:44 +0300

    codecrafters submit [skip ci]

--- app/HistoryCommand.php
+++ app/HistoryCommand.php
@@ -6,7 +6,9 @@ readonly class HistoryCommand extends AbstractCommand
 {
     public function execute(): void
     {
-        foreach (readline_list_history() as $index => $prevCommand) {
+        $offset = (int) ($this->args[0] ?? 0);
+        $prevCommands = array_slice(readline_list_history(), -$offset, null, true);
+        foreach ($prevCommands as $index => $prevCommand) {
             $line = sprintf('%5d  %s', $index + 1, $prevCommand);
             fwrite($this->out, $line . PHP_EOL);
         }
